4e4dc791ec80fd97987b450e225454b828023be8
brooky56 <aidarahmetshin56@gmail.com>
2020-03-09 22:33:55 +0300

    Added solutions to 1 and 3

--- HomeWork_2/Task1.py
+++ HomeWork_2/Task1.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from math import *
+import statistics
 
 # current dir
 curr_work_dir = os.getcwd()
@@ -25,5 +26,55 @@ def read_dataset(path):
     file.close()
 
 
+def plot_before():
+    t = np.array([data_arr[i][0] for i in range(len(data_arr))])
+    angle = np.array([data_arr[i][1] for i in range(len(data_arr))])
+    print("Amount of elements: {0}".format(len(angle)))
+
+    angle_mean = statistics.mean(angle)
+    angle_sdev = statistics.stdev(angle)
+
+    print("Mean: {0}".format(angle_mean))
+    print("Standart deviation: {0}".format(angle_sdev))
+
+    plt.plot(t, angle)
+    plt.title("Angles(t) values before")
+    plt.xlabel("Time")
+    plt.ylabel("Angel")
+    plt.show()
+
+    return angle_mean, angle_sdev, angle
+
+
+std_e = 0.003
+std_n = 0.2
+
+E = std_n ** 2
+
+signal = []
+prediction = []
+Ks = []
+
 if __name__ == '__main__':
     read_dir("case3.txt", curr_work_dir)
+    plot_before()
+
+    x0 = data_arr[0][0]
+    y0 = data_arr[0][1]
+
+    i = 1
+    for i in range(len(data_arr)):
+        dx = data_arr[i][0] - x0
+        x0 = data_arr[i][0]
+        K = (E + std_e ** 2) / (E + std_n ** 2 + std_e ** 2)
+        E = std_n ** 2 * (E + std_e ** 2) / (E + std_n ** 2 + std_e ** 2)
+
+        y0 = K * data_arr[i][1] + (1 - K) * (y0 + 5 * dx)
+
+        Ks.append(K)
+        prediction.append(y0)
+        signal.append(data_arr[i][1])
+
+    plt.plot(signal, label='data', )
+    plt.plot(prediction, label='filtered', c='black')
+    plt.show()
